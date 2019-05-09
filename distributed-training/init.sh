08bb4bd164015fdd1d6189bb760e58b8cca61f6d
Eric Caron <eric.caron@emc.com>
2019-05-09 08:41:23 -0400

    modified init script to download ignite instead of compiling

--- distributed-training/init.sh
+++ distributed-training/init.sh
@@ -1,41 +1,38 @@
-if [ -d ./ignite ]
-then
+
+if [ -f ./apache-ignite-2.7.0-bin.zip ] ; then
     echo "Apache Ignite already downloaded..."
 else
-    echo "Downloading Apache Ignite..."
-    git clone https://github.com/apache/ignite.git
-    cd ignite
-    git checkout ea33ec7f0af8fcad113cd92953fba0e8e5502dfa
-    cd ..
+    wget http://mirrors.advancedhosters.com/apache//ignite/2.7.0/apache-ignite-2.7.0-bin.zip 
 fi
 
-if [ -d ./apache-ignite-2.7.0-SNAPSHOT-bin ]
-then
-    echo "Apache Ignite already built..."
-else
-    echo "Building Apache Ignite..."
-    cd ignite
-    mvn clean package -q -B -DskipTests -Prelease
 
+if [ -d ./apache-ignite-2.7.0-bin ]
+then
+    echo "Apache Ignite already in place ..."
+else
     echo "Unzipping Apache Ignite package..."
-    cd ..
-    mv ignite/target/bin/apache-ignite-2.7.0-SNAPSHOT-bin.zip ./
-    unzip -qo apache-ignite-2.7.0-SNAPSHOT-bin.zip
-    rm apache-ignite-2.7.0-SNAPSHOT-bin.zip
+    unzip -qo apache-ignite-2.7.0-bin.zip
+    #rm apache-ignite-2.7.0-SNAPSHOT-bin.zip
 fi
 
 echo "Updating path..."
-cd apache-ignite-2.7.0-SNAPSHOT-bin/bin
+cd apache-ignite-2.7.0-bin/bin
 export PATH=`pwd`:$PATH
 cd ../..
+
+
 
 echo "Downloading cifar10..."
 python3 cifar10_download_and_extract.py
 
-echo "Downloading models..."
-rm -rf models
-git clone https://github.com/tensorflow/models.git --depth 1 --branch r1.13.0
-rm -rf models/research models/samples models/tutorials models/.git
-patch -p0 < models.diff
+if [ -d models ] ; then
+    echo "models is already present"
+else
+    echo "Downloading models..."
+    git clone https://github.com/tensorflow/models.git --depth 1 --branch r1.13.0
+    rm -rf models/research models/samples models/tutorials models/.git
+    patch -p0 < models.diff
+fi
 
 echo "Initialization completed"
+
